feb45c5fee0bfa981c1fa7993a1a6aa0e4be566a
Martha Husar <martahusar757@gmail.com>
2023-03-14 02:17:39 +0300

    added Prototype of Controller

---
+++ Sense-Capital-Test-Task/Controllers/GameController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SenseCapital.Model;
+
+namespace Sense_Capital_Test_Task.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GameController : ControllerBase
+    {
+
+        [HttpGet]
+        public IActionResult GetGames()
+        {
+            return Ok();
+
+
+        }
+        [HttpGet("{id:int}")]
+        public IActionResult GetGamesById(int id)
+        {
+            return Ok();
+
+        }
+
+        [HttpPost]
+        public IActionResult PostTurn(Game game)
+        {
+            return Ok();
+
+
+        }
+
+        [HttpPut("{id:int}")]
+
+        public IActionResult PutTurnById(int Id, Game game)
+        {
+            return Ok();
+
+
+        }
+        //
+        [HttpDelete("{id:int}")]
+        public IActionResult ClearField(int id)
+        {
+            return Ok();
+
+
+        }
+
+    }
+}
---
+++ SenseCapital.Model/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace SenseCapital.Model
+{
+    public class Game
+    {
+        public int Id { get; set; }
+        public  bool?[][] Field { get; set; } 
+
+        public int KeyOfFirstPlayer { get; set; }
+        public int KeyOfSecondPlayer { get; set; }
+     
+    }
+}
